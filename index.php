63def2274c00089477f3b4bb91984ebf1070bd71
Favour919 <favourbassey999@gmail.com>
2022-10-11 12:14:03 +0100

    new

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 
 <head>
-    <title>Real Programmer: User Register</title>
+    <title>User Register</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
@@ -12,7 +12,7 @@
 <body>
     <br />
     <div class="container">
-        <h2 align="center"> User Register</h2>
+        <h2 align="center"> User Registration</h2>
         <br />
         <div class="col-md-6" style="margin:0 auto; float:none;">
             <form method="post" action="user_data.php">
